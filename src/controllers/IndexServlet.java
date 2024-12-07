d4d64687a2eb891eee932bc149637ed43703bb81
momoko.hongou <shinbapit@gmail.com>
2024-12-07 17:50:23 +0900

    IndexServlet.javaを作成

---
+++ src/controllers/IndexServlet.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet{
+	private static final longserialVersionUID=1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+
+}
