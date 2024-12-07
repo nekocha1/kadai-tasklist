37a81194c6a8d8a4adb769203368b50ba2e2a5d4
momoko.hongou <shinbapit@gmail.com>
2024-12-07 18:19:59 +0900

    データベース関連のファイルを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,18 +1,26 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Task;
+import util.DBUtil;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet{
-	private static final longserialVersionUID=1L;
+	private static final long serialVersionUID=1L;
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
+		List<Task> tasks = em.createNamedQuery("getAllTasks",Task.class).getResultList();
+		resp.getWriter().append(Integer.valueOf(tasks.size()).toString());
 	}
 
 }
